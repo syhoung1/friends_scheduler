32544c8c0fbefca795621b0372485ec21273e2a1
Steven <syhoung1@gmail.com>
2018-07-26 01:55:15 +0000

    setup for databases and views

--- settings.py
+++ settings.py
@@ -77,8 +77,14 @@ WSGI_APPLICATION = 'schedules.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'users',
+        'USER': 'steven',
+        'HOST': 'localhost',
+        'PORT': '',
+        'TEST': {
+            'NAME': 'users_test',
+        },
     }
 }
 
